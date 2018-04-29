613f7d6c45a5a9fff20789afd9b236a2f19b1367
Tabish Bashir <tabishbashir@gmail.com>
2018-04-29 13:43:48 +0530

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const Gameboard = ( ()=> {
+
+  let board = [ [null, null, null],
+                [null, null, null],
+                [null, null, null] ]
+
+  const printBoard = () => {
+    for (var i = 0; i < 3; i++) {
+      console.log(board[i][0],board[i][1],board[i][2]);
+    }
+  }
+
+  return {printBoard}
+
+})();
+
+let box = document.querySelector(".box");
+console.log(box.value);
